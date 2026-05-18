309d45189f5ee36028580586fe46b85d5eb34836
shalabhraj1990 <shalabhrajetc@gmail.com>
2026-05-18 07:27:18 +0530

    addded textbox test cases

--- tests/uiComponet.spec.ts
+++ tests/uiComponet.spec.ts
@@ -56,3 +56,22 @@ test.describe("Form Layout Page", () => {
     ).toBeChecked();
   });
 });
+
+test("CheckBoxes", async ({ page }) => {
+  await page.getByText("Modal & Overlays").click();
+  await page.getByText("Toastr").click();
+
+  await page
+    .getByRole("checkbox", { name: "Hide on click" })
+    .uncheck({ force: true });
+
+  await page
+    .getByRole("checkbox", { name: "Prevent arising of duplicate toast" })
+    .check({ force: true });
+
+  const allBoxes = page.getByRole("checkbox");
+  for (const box of await allBoxes.all()) {
+    await box.check({ force: true });
+    expect(await box.isChecked()).toBeTruthy();
+  }
+});
